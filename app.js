a2a7c152d859a907f145c4ab52cbf26b464c4868
korpunek <leszek@dim.pl>
2022-12-18 21:24:22 +0100

    pierwszy commit

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const port = 3000
+
+
+const app = express()
+app.set('view engine', 'hbs')
+
+app.get('/', function(req, res) {
+
+    res.send('<h1>Strona z EXPRESS</h1>')
+})
+
+app.get('/about', function(req, res) {
+
+    res.render('index', {
+        tytul: 'strona z exxpres',
+        tresc: 'strona testowa'
+    })
+})
+
+
+app.listen(port)
